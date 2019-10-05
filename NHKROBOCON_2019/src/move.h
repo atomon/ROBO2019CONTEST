7b6c4c807561a94f81e6219fc887c332f508c55b
cycling-Ohnishi <cycling-Ohnishi@users.noreply.github.com>
2019-10-05 11:14:27 +0900

    add feedback function

--- NHKROBOCON_2019/src/move.h
+++ NHKROBOCON_2019/src/move.h
@@ -23,7 +23,7 @@ using namespace Eigen;
 
 //declare object
 TEENSY_CAN can(1000000); // baud setting
-PID_VECTOR4F pid_vec4f(matrin(arrays));
+PID_VECTOR4F pid_wheel(matrin(arrays));
 EIGEN_CALC eigen_calc;
 
 class MOVE
@@ -43,11 +43,11 @@ class MOVE
         
         Vector4f vec_odometry, pre_vec_odometry;
         
-        Vector3f vec_location;
+        Vector3f vec_location = Vector3f::Zero();
         
         Vector3f vec_location_target;
         
-        Vector4f vec_target_rpm, vec_wheel_outrpm;
+        Vector4f vec_target_rpm = Vector4f::Zero(), vec_wheel_outrpm = Vector4f::Zero();
 
         float odometry_R = 0.159592907;
 
@@ -57,7 +57,7 @@ class MOVE
 
         float pre_theta = 0.0;
 
-        int i = 1;
+        int i = 0;
 
     public:
 
@@ -65,7 +65,9 @@ class MOVE
 
         void set_target(vector<int> _x, vector<int> _y, float _theta);
 
-        bool update();
+        void target_update();
+
+        void feedback();
 
         bool process_end();
 
@@ -82,46 +84,52 @@ void MOVE::set_target(vector<int> _x, vector<int> _y, float _theta)
     location_x = _x;
     location_y = _y;
     target_theta = _theta;
-    i = 1;
+    i = 0;
 }
 
-bool MOVE::update()
+void MOVE::target_update()
+{
+    //Read together two data with CAN
+    tie(odometry[1], odometry[2]) = can.read(3);
+    tie(odometry[0], odometry[3]) = can.read(4);
+    tie(theta, ignore) = can.read(21);
+
+    //Vector4f conversion
+    Map<Vector4f> vec_odometry(odometry);
+
+    //LSB
+    theta /= 100000;
+    theta *= PI/180;
+
+    //current location
+    //vec_location << odometry[1] * (odometry_R / 8192.0), odometry[0] * (odometry_R / 8192.0), theta;
+    vec_location += eigen_calc.location((vec_odometry - pre_vec_odometry) * (odometry_R / 8192.0), theta); 
+    vec_location[2] = theta; // Using Gyro
+
+    //target location
+    //if(++i > (int)location_y.size()-1) i = (int)location_y.size()-1; //最初に足し算してるので目標座標の０番は実行されない
+    if(++i > (int)location_y.size()-1) i = (int)location_y.size()-1; //最初に足し算してるので目標座標の０番は実行されない
+    vec_location_target << location_x[i] * (odometry_R / 8192.0), location_y[i] * (odometry_R / 8192.0), target_theta;
+
+    //各ホイールの回転数を計算
+    vec_target_rpm = eigen_calc.wheel(vec_location, vec_location_target);
+
+    pre_vec_odometry = vec_odometry;
+    pre_theta = theta;
+}
+
+void MOVE::feedback()
 {
     //Read together two data with CAN
     tie(wheel_rpm[0], wheel_rpm[1]) = can.read(1);  // tie(Value, Value2) = object name.read(ID);
     tie(wheel_rpm[2], wheel_rpm[3]) = can.read(2);
-    tie(odometry[1], odometry[0]) = can.read(3);
-    tie(odometry[3], odometry[2]) = can.read(4);
-    tie(theta, ignore) = can.read(21);
-    //tie(analog_r[0], analog_r[1]) = can.read(22);
-    //tie(analog_r[2], analog_r[3]) = can.read(23);
 
     //Vector4f conversion
     Map<Vector4f> vec_wheel_rpm(wheel_rpm);
-    Map<Vector4f> vec_odometry(odometry);
 
-    //current location
-    theta /= 100000;
-    theta *= PI/180;
-    //vec_location << odometry[0] * (odometry_R / 8192.0), odometry[1] * (odometry_R / 8192.0), theta;
-    vec_location += eigen_calc.location((vec_odometry - pre_vec_odometry) * (odometry_R / 8192.0), theta); //(vec_odometry - pre_vec_odometry) * (odometry_R / 8192.0), theta
-    vec_location[2] = theta;
-
-    //target location
-    if(i > (int)location_y.size()-1) i = (int)location_y.size()-1;
-    vec_location_target << location_x[i] * (odometry_R / 8192.0), location_y[i] * (odometry_R / 8192.0), target_theta;
-
-    //目標値についたか？
-    if(MOVE::process_end() == true){
-        MOVE::stop();
-        pid_vec4f.reset_i();
-        return true;
-    } 
-
-    //各ホイールの回転数を計算
-    vec_target_rpm = eigen_calc.wheel(vec_location, vec_location_target);
-    pid_vec4f.update(vec_wheel_rpm, vec_target_rpm);
-    vec_wheel_outrpm = pid_vec4f.result_val();
+    //calculation PID
+    vec_wheel_outrpm = pid_wheel.update(vec_wheel_rpm, vec_target_rpm, wheel_interrupt_timer);
+    // vec_wheel_outrpm = pid_wheel.result_val();
 
     //Vector4f型をint型に変換
     //output_wheel = Vector4f_int(vec_output_wheel); // pointa
@@ -129,19 +137,13 @@ bool MOVE::update()
 
     //send data for motor
     can.dual_send(ID_MDD, wheel_outrpm, 4);
-
-    i++;
-    pre_vec_odometry = vec_odometry;
-    pre_theta = theta;
-    
-    return 0;
 }
 
 bool MOVE::process_end()
 {
     int val = 0b000;
-    val |= ((odometry[0] < location_x.back() + error_range) && (odometry[0] > location_x.back() - error_range))? 0b001 : 0b000;
-    val |= ((odometry[1] < location_y.back() + error_range) && (odometry[1] > location_y.back() - error_range))? 0b010 : 0b000;
+    val |= ((vec_location[0] < location_x.back() * (odometry_R / 8192.0) + error_range) && (vec_location[0] > location_x.back() * (odometry_R / 8192.0) - error_range))? 0b001 : 0b000;
+    val |= ((vec_location[1] < location_y.back() * (odometry_R / 8192.0) + error_range) && (vec_location[1] > location_y.back() * (odometry_R / 8192.0) - error_range))? 0b010 : 0b000;
     val |= ((theta < target_theta + error_range_angle) && (theta > target_theta - error_range_angle))? 0b100 : 0b000;
     
     return val == 0b111 ? true : false;
@@ -190,9 +192,7 @@ void MOVE::print(int num)
             Serial.print(" ");
             Serial.print(vec_location[1],5);
             Serial.print(" ");
-            Serial.print(odometry[1] * (odometry_R / 8192.0),5);
-            Serial.print(" ");
-            Serial.print(odometry[0] * (odometry_R / 8192.0),5);
+            Serial.print(theta,5);
             Serial.print(" ");
     }
     //Serial.println();
